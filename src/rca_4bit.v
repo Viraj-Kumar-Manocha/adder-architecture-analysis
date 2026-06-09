4c6b96a9e73a3e2c9f2dae9aa0f0b860122dbb5b
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-06-10 02:57:47 +0530

    Update rca_4bit.v

--- src/rca_4bit.v
+++ src/rca_4bit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2026 01:56:05
-// Design Name: 
-// Module Name: 4bit_RCA
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module rca_4bit(input [3:0]a,
                 input [3:0]b,
