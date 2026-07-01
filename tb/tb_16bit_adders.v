64e53bfb597f191e74022be273d691b241972c0a
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 17:12:53 +0530

    Update and rename tb_16bit_adders.v to tb/tb_16bit_adders.v

--- tb/tb_16bit_adders.v
+++ tb/tb_16bit_adders.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.06.2026 03:21:43
-// Design Name: 
-// Module Name: tb_16bit_adders
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_16bit_adders;
 reg Cin;
