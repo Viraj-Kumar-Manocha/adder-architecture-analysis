9a18a04f74cfe54b1391bd90cb1257f213f270cf
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 17:09:19 +0530

    Update and rename tb_64bit_adders.v to tb/tb_64bit_adders.v

--- tb/tb_64bit_adders.v
+++ tb/tb_64bit_adders.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.06.2026 03:22:03
-// Design Name: 
-// Module Name: tb_64bit_adders
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_64bit_adders;
 reg [63:0] a,b;
